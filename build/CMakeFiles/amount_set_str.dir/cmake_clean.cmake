bb252139505fbd55fad596be82b9a33446ae83f5
maorben <maorben241@gmail.com>
2021-11-27 18:47:37 +0200

    changes benyamin made

---
+++ build/CMakeFiles/amount_set_str.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/amount_set_str.dir/amount_set_str.c.o"
+  "CMakeFiles/amount_set_str.dir/linkedList.c.o"
+  "CMakeFiles/amount_set_str.dir/tests.c.o"
+  "amount_set_str"
+  "amount_set_str.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/amount_set_str.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
